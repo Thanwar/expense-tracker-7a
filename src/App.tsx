3364d585d770c01b1e638071778762bf4f9f4f09
Thanwar <thawardas25@gmail.com>
2020-12-26 00:32:42 +0500

    added notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import './App.css';
+import "./App.css";
+import { GlobalProvider } from "./context/GlobalState";
+import firebase from "./firebase";
+
+// Components
+import { Header         } from "./components/Header";
+import { Balance        } from "./components/Balance";
+import { AmountSummary  } from "./components/AmountSummary";
+import { History        } from "./components/History";
+import { AddTransaction } from "./components/AddTransaction";
 
 function App() {
+
+  const messaging = firebase.messaging();
+  messaging.requestPermission().then(()=>{
+    return messaging.getToken();
+  }).then((token)=>{
+    console.log('token',token);
+    
+  })
+
+
+
+
   return (
-    <div className="App">
-    <h1>Pwa</h1>
+    <div>
+      <GlobalProvider>
+        <Header />
+        <div className="container">
+          <Balance />
+          <AmountSummary />
+          <History />
+          <AddTransaction />
+        </div>
+      </GlobalProvider>
     </div>
   );
 }
---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+ import firebase from 'firebase';
+ 
+ 
+ // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB4byOIjkR5rBQ8iCAzuGybJkuCO3bCWYA",
+    authDomain: "expenceker.firebaseapp.com",
+    projectId: "expenceker",
+    storageBucket: "expenceker.appspot.com",
+    messagingSenderId: "234618435074",
+    appId: "1:234618435074:web:25d10ce493e0bf48ff591b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase;
